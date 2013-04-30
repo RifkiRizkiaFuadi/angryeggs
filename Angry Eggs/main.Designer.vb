5acb1d9606a5e28b8b03a17ae81b4732cabaa15e
Rifki Rizkia Fuadi <mail@rifki.my.id>
2013-04-30 23:15:25 +0700

    First Upload
    Angry Eggs VB base game

---
+++ Angry Eggs/main.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.scoreLabel = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RestartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.restart = New System.Windows.Forms.Button()
+        Me.ExitGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.bat = New System.Windows.Forms.PictureBox()
+        Me.egg = New System.Windows.Forms.PictureBox()
+        Me.bg = New System.Windows.Forms.PictureBox()
+        Me.bgbottom = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.bat, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.egg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bgbottom, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'scoreLabel
+        '
+        Me.scoreLabel.BackColor = System.Drawing.Color.White
+        Me.scoreLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.scoreLabel.Location = New System.Drawing.Point(529, 6)
+        Me.scoreLabel.Name = "scoreLabel"
+        Me.scoreLabel.Size = New System.Drawing.Size(66, 30)
+        Me.scoreLabel.TabIndex = 2
+        Me.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RestartToolStripMenuItem, Me.ExitGameToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(127, 48)
+        '
+        'RestartToolStripMenuItem
+        '
+        Me.RestartToolStripMenuItem.Name = "RestartToolStripMenuItem"
+        Me.RestartToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.RestartToolStripMenuItem.Text = "Restart"
+        '
+        'restart
+        '
+        Me.restart.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.restart.Location = New System.Drawing.Point(10, 9)
+        Me.restart.Name = "restart"
+        Me.restart.Size = New System.Drawing.Size(113, 23)
+        Me.restart.TabIndex = 7
+        Me.restart.Text = "RESTART GAME"
+        Me.restart.UseVisualStyleBackColor = True
+        '
+        'ExitGameToolStripMenuItem
+        '
+        Me.ExitGameToolStripMenuItem.Name = "ExitGameToolStripMenuItem"
+        Me.ExitGameToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.ExitGameToolStripMenuItem.Text = "Exit Game"
+        '
+        'bat
+        '
+        Me.bat.BackgroundImage = Global.Angry_Eggs.My.Resources.Resources.bat
+        Me.bat.Location = New System.Drawing.Point(191, 474)
+        Me.bat.Name = "bat"
+        Me.bat.Size = New System.Drawing.Size(180, 26)
+        Me.bat.TabIndex = 6
+        Me.bat.TabStop = False
+        '
+        'egg
+        '
+        Me.egg.BackColor = System.Drawing.Color.Transparent
+        Me.egg.Image = Global.Angry_Eggs.My.Resources.Resources.egg
+        Me.egg.Location = New System.Drawing.Point(340, 417)
+        Me.egg.Name = "egg"
+        Me.egg.Size = New System.Drawing.Size(40, 40)
+        Me.egg.TabIndex = 4
+        Me.egg.TabStop = False
+        '
+        'bg
+        '
+        Me.bg.BackgroundImage = Global.Angry_Eggs.My.Resources.Resources.bg
+        Me.bg.Location = New System.Drawing.Point(0, 1)
+        Me.bg.Name = "bg"
+        Me.bg.Size = New System.Drawing.Size(600, 39)
+        Me.bg.TabIndex = 3
+        Me.bg.TabStop = False
+        '
+        'bgbottom
+        '
+        Me.bgbottom.BackgroundImage = Global.Angry_Eggs.My.Resources.Resources.bg2
+        Me.bgbottom.Location = New System.Drawing.Point(0, 461)
+        Me.bgbottom.Name = "bgbottom"
+        Me.bgbottom.Size = New System.Drawing.Size(600, 39)
+        Me.bgbottom.TabIndex = 5
+        Me.bgbottom.TabStop = False
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(600, 500)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.restart)
+        Me.Controls.Add(Me.bat)
+        Me.Controls.Add(Me.egg)
+        Me.Controls.Add(Me.scoreLabel)
+        Me.Controls.Add(Me.bg)
+        Me.Controls.Add(Me.bgbottom)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Angry Eggs"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.bat, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.egg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bgbottom, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents scoreLabel As System.Windows.Forms.Label
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents RestartToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents bg As System.Windows.Forms.PictureBox
+    Friend WithEvents egg As System.Windows.Forms.PictureBox
+    Friend WithEvents bgbottom As System.Windows.Forms.PictureBox
+    Friend WithEvents bat As System.Windows.Forms.PictureBox
+    Friend WithEvents restart As System.Windows.Forms.Button
+    Friend WithEvents ExitGameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Angry Eggs/main.vb
@@ -0,0 +1,167 @@
+﻿Public Class main
+    Dim hSpeed As Single = -2
+    Dim vSpeed As Single = -2
+    Dim rows As Integer = 5
+    Dim cols As Integer = 10
+    Dim topRow As Single = 0.1
+    Dim rowHeight As Single = 0.1
+    Dim score As Integer = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Mengecek bila telur menyentuh bagian atas window
+        If egg.Top < 39 Then
+            vSpeed = -vSpeed
+        End If
+
+        'Mengecek bila telur menyentuh bagian kiri window
+        If egg.Left < 0 Then
+            hSpeed = -hSpeed
+        End If
+
+        'Mengecek bila telur menyentuh bagian bawah window
+        If egg.Bottom > Me.ClientRectangle.Height Then
+            Timer1.Enabled = False
+            egg.Image = My.Resources.eggbreak
+            restart.Text = "NEW GAME"
+            My.Computer.Audio.Play(Application.StartupPath & "\lose.wav")
+            MsgBox("GAME OVER !!!" & vbNewLine & vbNewLine & "Your Score : " & score)
+        End If
+
+        Dim C As Single = egg.Left + egg.Width / 2
+        'Mengecek bila telur menyentuh bat
+        If C > bat.Left And C < bat.Right And vSpeed > 0 And egg.Bottom > bat.Top And egg.Top < bat.Top Then
+            vSpeed = -vSpeed
+            My.Computer.Audio.Play(Application.StartupPath & "\shot.wav")
+
+            'Membelokkan bola
+            Dim offset As Single = (egg.Left + egg.Width / 2) - (bat.Left + bat.Width / 2)
+            Dim ratio As Single = offset / (bat.Width / 2)
+            hSpeed += 3 * ratio
+        End If
+
+        'Mengecek bila telur menyentuh bagian kanan windows
+        If egg.Right > Me.ClientRectangle.Width - 0 Then
+            hSpeed = -hSpeed
+        End If
+
+        For Each cnt As Control In Me.Controls
+            If cnt.Name = "brick" Then
+                checkBrick(cnt, egg)
+            End If
+        Next
+
+        egg.Left += hSpeed
+        egg.Top += vSpeed
+    End Sub
+
+    Private Sub checkBrick(brick As PictureBox, ByVal egg As PictureBox)
+        If brick.Visible Then
+            Dim hit As Boolean = False
+            Dim C As Single = egg.Left + egg.Width / 2
+
+            'Mengecek bila telur menyentuh bagian bawah brick
+            If vSpeed < 0 And C > brick.Left And C < brick.Right And egg.Top < brick.Bottom And egg.Bottom > brick.Bottom Then
+                vSpeed = -vSpeed
+                hit = True
+            End If
+
+            'Mengecek bila telur menyentuh bagian atas brick
+            If vSpeed > 0 And C > brick.Left And C < brick.Right And egg.Bottom > brick.Top And egg.Top < brick.Top Then
+                vSpeed = -vSpeed
+                hit = True
+            End If
+
+            C = egg.Top + egg.Height / 2
+            'Mengecek bila telur menyentuh bagian kiri brick
+            If hSpeed > 0 And C > brick.Top And C < brick.Bottom And egg.Right > brick.Left And egg.Left < brick.Left Then
+                hSpeed = -hSpeed
+                hit = True
+            End If
+
+            'Mengecek bila telur menyentuh bagian kanan brick
+            If hSpeed < 0 And C > brick.Top And C < brick.Bottom And egg.Left < brick.Right And egg.Right > brick.Right Then
+                hSpeed = -hSpeed
+                hit = True
+            End If
+
+            If hit Then
+                brick.Visible = False
+                score += brick.Tag
+                My.Computer.Audio.Play(Application.StartupPath & "\oink.wav")
+                scoreLabel.Text = score.ToString
+                If score = 150 Then
+                    Timer1.Stop()
+                    restart.Text = "NEW GAME"
+                    My.Computer.Audio.Play(Application.StartupPath & "\win.wav")
+                    MsgBox("CONGRATULATION !!!" & vbNewLine & vbNewLine & "You Got Perfect Score : 150")
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub main_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        bat.Width = 0.3 * Me.ClientRectangle.Width
+        bat.Height = 0.05 * Me.ClientRectangle.Height
+        bat.Top = 0.95 * Me.ClientRectangle.Height
+        bat.Left = e.X - (0.15 * Me.ClientRectangle.Width)
+    End Sub
+
+    Private Sub makeBrick()
+        'menghapus brick lama
+        For i As Integer = Me.Controls.Count - 1 To 0 Step -1
+            If Me.Controls(i).Name = "brick" Then
+                Me.Controls.RemoveAt(i)
+            End If
+        Next
+
+        'membuat brick baru
+        For r As Integer = 0 To rows - 1
+            For c As Integer = 0 To cols - 1
+                Dim b As New PictureBox
+                Me.Controls.Add(b)
+                b.Visible = True
+                b.Name = "brick"
+                b.Tag = rows - r
+                b.Width = Me.ClientRectangle.Width / cols
+                b.Height = Me.ClientRectangle.Height * rowHeight
+                b.Left = c * b.Width
+                b.Top = Me.ClientRectangle.Height * (topRow + r * rowHeight)
+                If (r + c) Mod 2 = 0 Then
+                    b.BackgroundImage = My.Resources.pig1
+                Else
+                    b.BackgroundImage = My.Resources.pig2
+                End If
+            Next
+        Next
+
+        With egg
+            .Left = Me.ClientRectangle.Width / 2
+            .Top = Me.ClientRectangle.Height * 0.9
+            vSpeed = -3
+            hSpeed = 1
+        End With
+        Timer1.Enabled = True
+        egg.Image = My.Resources.egg
+    End Sub
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        makeBrick()
+        egg.Left = Me.ClientRectangle.Width / 2
+        egg.Top = Me.ClientRectangle.Height * 0.9
+    End Sub
+
+    Private Sub RestartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RestartToolStripMenuItem.Click
+        makeBrick()
+        score = 0
+    End Sub
+
+    Private Sub ExitGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitGameToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub restart_Click(sender As Object, e As EventArgs) Handles restart.Click
+        makeBrick()
+        score = 0
+        scoreLabel.Text = "0"
+    End Sub
+End Class
